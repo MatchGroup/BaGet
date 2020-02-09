f905aff757467f861222fdfe4fed2a7927668708
David Penton <davidpenton-match@users.noreply.github.com>
2020-02-09 15:56:51 -0600

    Remove comments

--- src/publish.bat
+++ src/publish.bat
@@ -3,8 +3,6 @@ setlocal EnableDelayedExpansion
 
 if [%1]==[] goto BLANK
 
-rem dotnet restore BaGet
-rem dotnet build BaGet -c Release -o .\app\%1
 dotnet publish BaGet -c Release -o .\app\%1 --self-contained -r %1
 
 GOTO DONE
